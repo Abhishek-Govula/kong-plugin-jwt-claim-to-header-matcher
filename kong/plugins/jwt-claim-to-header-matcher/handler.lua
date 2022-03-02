939a5fca64117ee5751aa0919c0e53cb5ac5af8a
nagasai_govula <nagasai_govula@infosys.com>
2022-03-03 01:56:39 +0530

    updated the handler

--- kong/plugins/jwt-claim-to-header-matcher/handler.lua
+++ kong/plugins/jwt-claim-to-header-matcher/handler.lua
@@ -147,8 +147,6 @@ function plugin:access(plugin_conf)
 
   -- your custom code here
   kong.log.inspect(plugin_conf)   -- check the logs for a pretty-printed config!
-  kong.service.request.set_header(plugin_conf.request_header, "this is on a request")
-
   -- Reading the access token
   do_wid_validation(plugin_conf)
 
